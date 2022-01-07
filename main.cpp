91c02101a2ea03d3089a7bb20abda22cae88b4db
Lucas Ikuhara <lri2911@gmail.com>
2022-01-06 22:41:28 -0300

    refactor: extract code into functions fix: time() syntax errors

--- main.cpp
+++ main.cpp
@@ -5,6 +5,132 @@
 #include <time.h>
 
 
+// Variável do exercício
+long comandoParaExecutar = 0;
+
+
+// Cria processo filho
+int criarFilho() {
+
+    // Cria um processo filho
+    int pid = fork ();
+
+    if (pid < 0) {
+
+        // A função fork tem retorno negativo em caso de falhas
+        std::cout << "Falha ao criar o processo filhos" << " \n";
+        exit(1); // Finaliza o programa com código 1
+    }
+    
+    return pid;
+};
+
+
+// Realiza um fork e retorna o array de descritores 
+void criarFork(int descritorDePipe[]) {
+
+    // Trecho baseado no exemplo da aula 1 do conjunto 2 
+    // Cria o pipe
+    if(pipe(descritorDePipe) < 0) {
+
+        // A função pipe tem retorno negativo em caso de falhas
+        std::cout << "Falha ao criar o pipe" << " \n";
+        exit(1); // Finaliza o programa com código 1
+    }
+
+    std::cout << "Pipe criado." << " \n";
+}
+
+
+// Fecha as pontas do pipe
+void fecharPipes(int* descritorDePipe) {
+
+    close (descritorDePipe[0]);
+    close (descritorDePipe[1]);
+}
+
+
+void tarefaUm() {
+
+   // int* descritorDePipe;
+    int descritorDePipe[2];
+    criarFork(descritorDePipe);
+
+    // Cria processo filho
+    int pid = criarFilho();
+
+    // Código do processo pai
+    if (pid > 0) {
+
+        // Trecho baseado de
+        // https://www.ibm.com/docs/en/zos/2.1.0?topic=functions-wait-wait-child-process-end
+        wait(NULL); // Espera até que o processo filho retorne
+
+        read(descritorDePipe[0], &comandoParaExecutar, 32); // Leitura do pipe
+
+        // Fecha as pontas do pipe
+        fecharPipes(descritorDePipe);
+    }
+
+    // Código do processo filho
+    else {
+
+        // Trechos de https://www.geeksforgeeks.org/time-function-in-c/
+        std::cout << "lendo tempo.. " << " \n";
+        
+        time_t tempo;
+        // Ler segundos
+        tempo = time(NULL);
+
+        std::cout << " .. " << " \n";
+
+
+        // Imprime o tempo
+        std::cout << "Tempo: " << " \n";
+        std::cout << tempo << " \n";
+
+        // Escreve no pipe
+        // Como time_t é um long int
+        write (descritorDePipe[1], &tempo, sizeof(tempo));
+
+        // Fecha as pontas do pipe
+        fecharPipes(descritorDePipe);
+
+        exit(0);
+    }
+
+};
+
+
+void tarefaDois() {
+
+   // int* descritorDePipe;
+    int descritorDePipe[2];
+    criarFork(descritorDePipe);
+
+    // Cria processo filho
+    int pid = criarFilho();
+
+    // Código do processo pai
+    if (pid > 0) {
+
+        // Envia o comando por pipe
+        write (descritorDePipe[1], &comandoParaExecutar, sizeof(comandoParaExecutar));
+        
+        // Espera até que o processo filho retorne
+        wait(NULL); 
+    }
+
+    // Código do processo filho
+    else {
+        printf("Implementar");
+    }
+
+
+}
+
+
+
 // Fução que trata os sinais
 void tratarSinal(int sinal) {
 
@@ -26,74 +152,13 @@ void tratarSinal(int sinal) {
 
 };
 
-void tarefaUm(int *comandoParaExecutar) {
-
-    // Trecho baseado no exemplo da aula 1 do conjunto 2
-    int descritorDePipe[2];
-    
-    // Cria o pipe
-    if(pipe(descritorDePipe) < 0) {
-
-        // A função pipe tem retorno negativo em caso de falhas
-        std::cout << "Falha ao criar o pipe”" << " \n";
-        exit(1); // Finaliza o programa com código 1
-    }
-
-    // Cria um processo filho
-    int pid = fork ();
-
-    if (pid < 0) {
-
-        // A função fork tem retorno negativo em caso de falhas
-        std::cout << "Falha ao criar o processo filhos" << " \n";
-        exit(1); // Finaliza o programa com código 1
-    }
-
-    // Código do processo pai
-    if (pid > 0) {
-
-        // Trecho baseado de
-        // https://www.ibm.com/docs/en/zos/2.1.0?topic=functions-wait-wait-child-process-end
-        int *status;
-        wait(status); // Espera até que o processo filho retorne
-
-        read(descritorDePipe[0], comandoParaExecutar, 32); // Leitura do pipe
-
-        // Fecha as pontas do pipe
-        close (descritorDePipe[0]);
-        close (descritorDePipe[1]);
-    }
-
-    // Código do processo filho
-    else {
-
-        // Trechos de https://www.geeksforgeeks.org/time-function-in-c/
-        int tempo;
-        tempo = time(NULL); // Ler segundos
-
-        // Imprime o tempo
-        std::cout << time << " \n";
-
-        // Escreve no pipe
-        write (descritorDePipe[1], &tempo, 32);
-
-        // Fecha as pontas do pipe
-        close (descritorDePipe[0]);
-        close (descritorDePipe[1]);
-
-        exit(0);
-    }
-    
-
-};
 
 int main()
 {
     // Ler e imprimir o próprio PID
     int pid = getppid();
 
-    // Variável do exercício
-    int comandoParaExecutar;
+
 
     std::cout << pid << " \n";
     std::cout << "Iniciando tratador de sinais.. " << " \n";
@@ -109,9 +174,10 @@ int main()
     if (signal(SIGTERM, tratarSinal) == SIG_ERR)
         printf("Não foi possível capturar SIGTERM\n");
 
-    while (1) {
-        sleep(1);
-    }
+    // while (1) {
+    //     sleep(1);
+    // }
+    tarefaUm();
 
     return 0;
 }
