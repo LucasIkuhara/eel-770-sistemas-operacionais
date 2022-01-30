200e156e784cf3fc0409a4b7b472519031942cf0
Lucas Ikuhara <lri2911@gmail.com>
2022-01-30 03:44:29 -0300

    feat: add support to different sexes

--- trabalho_2/main.c
+++ trabalho_2/main.c
@@ -10,15 +10,16 @@ pthread_cond_t  semHomemCond = PTHREAD_COND_INITIALIZER;
 // Variável de condição para banheiro cheio
 pthread_cond_t  cheioCond = PTHREAD_COND_INITIALIZER;
 
-
 // Armazena a quantidade de pessoas no banheiro em um dado momento
 int qtdNoBanheiro = 0;
+
+// Armazena sexo usando o banheiro (0 indica mulher, 1 indica homem, 2 indica que está vazio)
+int sexoDoUsuario = 2;
 
 // Lock usada para restringir o acesso a variável contador
 pthread_mutex_t contadorLock = PTHREAD_MUTEX_INITIALIZER;
 
 // Locks que garantem que não existe ninguém do sexo oposto no banheiro
-
 pthread_mutex_t semMulherLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t semHomemLock = PTHREAD_MUTEX_INITIALIZER;
 
@@ -28,20 +29,19 @@ void usarBanheiro() {
     printf("Usando o banheiro.\n");
 };
 
-// Função que representa entrar no banheiro
-// Sexo 0 = Feminino
-// Sexo 1 = Masculino
-void entrarNoBanheiro(int sexo) {
+// Função que representa entrar no banheiro para threads mulher
+void entrarNoBanheiroMulher() {
 
-    // Caso o thread seja mulher
-    if (sexo == 0) {
-        while 
+    while (sexoDoUsuario != 0 || sexoDoUsuario != 2) {
+    
+        // Espera até que nenhum homem esteja no banheiro
+        pthread_cond_wait(&semHomemCond, &semHomemLock);
     }
-
-    // Caso o thread seja homem
-    else {
-
-    }
+    
+    // Ativa o lock semMulher, para que nenhum homem possa entrar e garante que a variável
+    // reflete a presença de uma mulher no banheiro
+    pthread_mutex_lock(&semMulherLock);
+    sexoDoUsuario = 0;
 
     // Threads esperam enquanto o banheiro está cheio (3 ou mais pessoas)
     while(qtdNoBanheiro >= 3) {
@@ -63,14 +63,27 @@ void entrarNoBanheiro(int sexo) {
         qtdNoBanheiro--;
         pthread_mutex_unlock(&contadorLock);
 
-        // Signal é chamado para acordar os threads esperando vaga no banheiro
+        // Se ninguém estiver no banheiro, desabilitar travas de sexo, e muda o indicador de flag para 2 (vazio)
+    	if (qtdNoBanheiro == 0) {
+            pthread_mutex_unlock(&semMulherLock);
+            sexoDoUsuario = 2;
+        }
+
+        // Signal é chamado para acordar um thread esperando que o último membro do sexo oposto deixe o banheiro
+        pthread_cond_signal(&semMulherCond);
+
+        // Signal é chamado para acordar um thread esperando vaga no banheiro
         pthread_cond_signal(&cheioCond);
 
+        // Ao chamarmos o signal do lock de sexo antes do de quantidade, criamos uma ideia de revezamento entre
+        // os sexos, visando evitar starvation
+
+        // Thread finaliza
+        pthread_exit(NULL);
 };
 
 int main(void) {
 
-    
 
     return 0;
 }
